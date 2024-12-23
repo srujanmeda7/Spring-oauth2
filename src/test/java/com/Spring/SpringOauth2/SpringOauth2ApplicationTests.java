4a3ca27fc3c3b75518cacfeed2e7eab4a8428dfa
srujanmeda7 <msrujan2002@gamil.in>
2024-12-23 11:55:13 +0530

    first commit

---
+++ src/test/java/com/Spring/SpringOauth2/SpringOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.SpringOauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
